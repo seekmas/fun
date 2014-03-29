38239476945fec049fb460ac700ad3a28568e4d7
System Administrator <root@motmatoiMac.local>
2014-03-29 23:27:25 +0800

    videojs font problem

--- app/cache/prod/twig/82/ab/fd186c6029bbf89bd54d8e61f52d9f20fa8c764c15b2df6a1a98c8dedc79.php
+++ app/cache/prod/twig/82/ab/fd186c6029bbf89bd54d8e61f52d9f20fa8c764c15b2df6a1a98c8dedc79.php
@@ -18,12 +18,9 @@ class __TwigTemplate_82abfd186c6029bbf89bd54d8e61f52d9f20fa8c764c15b2df6a1a98c8d
         // line 1
         echo "<h1>关于我们</h1>
 
-
-<p>我们是一个创业团队</p>
-
 <p>我们热爱自由</p>
 
-<p>我们欢迎所有的创业团队入驻</p>
+<p>欢迎所有的创业团队入驻</p>
 
 <p>我们开发免费的课程</p>
 
